54ae78d6c262c8f795a3e8a5e8105f3336f9410b
Marko2353 <57666712+Marko2353@users.noreply.github.com>
2023-02-23 12:29:15 +0100

    footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
+import Footer from "./components/footer/Footer";
 import Nav from "./components/nav/Nav";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <Nav />;
+  return (
+    <>
+      <Nav />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
