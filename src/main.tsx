40463b2535e5c74d5e61098115c2278802c8f3f9
Marko2353 <57666712+Marko2353@users.noreply.github.com>
2023-02-23 13:49:09 +0100

    apollo

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,41 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://rickandmortyapi.com/graphql",
+  cache: new InMemoryCache(),
+});
+
+// const client = ...
+
+client
+  .query({
+    query: gql`
+      query GetLocations {
+        locations {
+          id
+          name
+          description
+          photo
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result));
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
--- src/subpages/characters/Characters.tsx
+++ src/subpages/characters/Characters.tsx
@@ -1,8 +1,24 @@
 import LoadButton from "../../components/library/buttons/LoadButton/LoadButton";
 import Card from "../../components/library/visual/Card";
+import { useQuery, gql } from "@apollo/client";
+
+const GET_CHARACTERS = gql`
+  query GetCharacters {
+    characters {
+      id
+      name
+      description
+      photo
+    }
+  }
+`;
 
 export default function Characters() {
-  return (
+  const { loading, error, data } = useQuery(GET_CHARACTERS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+  return data.characters.map(({ id, name, description, photo }) => (
     <>
       <img src="./src/assets/PngItem_438051 1.png" alt="" />
       <ul>
@@ -15,5 +31,5 @@ export default function Characters() {
       </ul>
       <LoadButton />
     </>
-  );
+  ));
 }
