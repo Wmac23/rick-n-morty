ad8623382ef9d04bd1d2007ac780e927a0a27da1
Wmac23 <72547887+Wmac23@users.noreply.github.com>
2023-02-23 13:44:47 +0100

    Yes

---
+++ src/subpages/episodes/EpisodeDetail.tsx
@@ -0,0 +1,12 @@
+import LoadButton from "../../components/library/buttons/LoadButton/LoadButton";
+import Card from "../../components/library/visual/Card";
+export default function EpisodesDetail() {
+  return (
+    <>
+      <img src="./src/assets/PngItem_438051 1.png" alt="" />
+      <ul>
+        <Card />
+      </ul>
+    </>
+  );
+}
